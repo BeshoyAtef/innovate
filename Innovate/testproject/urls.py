a214fb217a6bbb0bf273490e7dddb4277532a8a7
AbdelrahmanAyad <abdelrahman.mayad@gmail.com>
2013-08-06 00:31:59 +0200

    added the missing urls and the new settings configurations

--- Innovate/testproject/urls.py
+++ Innovate/testproject/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -16,13 +16,18 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^email/', 'www.views.sendemail'),
     url(r'^Contact/', 'www.views.view_contact_us'),
-    # url(r'^test/', 'www.views.add_video'),
+    url(r'^test/', 'www.views.delete_post'),
+    url(r'^test1/', 'www.views.test1'),
     url(r'^base/', 'www.views.render_base'),
     url(r'^radio/', 'www.views.render_radioAds'),
-    # url(r'^add/', 'www.views.render_radioAds'),
     url(r'^add/video/', 'www.views.add_video'),
+    url(r'^/weddingclips/', 'www.views.show_wedding_videos'),
+    url(r'^/documentaryclips/', 'www.views.show_documentaries_videos'),
+    url(r'^/promoclips/', 'www.views.show_promo_videos'),
     url(r'^add/contact/', 'www.views.add_contact'),
-
-
-
+    url(r'^testincrement/', 'www.views.increment_number_of_views'),
 )
+
+urlpatterns += patterns('django.views.static',
+        (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),
+    )
