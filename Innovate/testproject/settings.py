7a5b684f38a1120bd6c66207ae14c1046086d842
Mohamed Ahmed Awad <mohamed@mohamed-pc.(none)>
2013-08-06 01:08:08 +0200

    added sorl app in settings.py and added its requirements' commands in shell for future refrences in readme.txt file

--- Innovate/testproject/settings.py
+++ Innovate/testproject/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'www',
     'soundcloud',
+    'sorl.thumbnail'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
